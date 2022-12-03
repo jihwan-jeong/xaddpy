aae76eb1e50807171b9f0e94f016cdf2a60893e8
jihwan-jeong <jiihwan.jeong@gmail.com>
2022-12-03 15:35:16 -0500

    minor version num fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='xaddpy',
     packages=['xaddpy'],
     install_requires=['numpy', 'sympy', 'pulp'],
-    version='v0.1.0',
+    version='0.1.0',
     license='MIT License',
     description='XADD package in Python',
     long_description=long_description,
@@ -17,5 +17,5 @@ setup(
     author_email='jiihwan.jeong@gmail.com',
     url='https://github.com/jihwan-jeong/xaddpy',
     keywords=["xadd", "xadd python", "symbolic diagram"],
-    download_url="https://github.com/jihwan-jeong/xaddpy/archive/refs/tags/v0.1.0.tar.gz"
+    download_url="https://github.com/jihwan-jeong/xaddpy/archive/refs/tags/0.1.0.tar.gz"
 )
