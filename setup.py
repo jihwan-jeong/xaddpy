6659e770a47c204a50072f6bd601851094bb82f6
jihwan-jeong <jiihwan.jeong@gmail.com>
2022-12-16 23:21:06 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='xaddpy',
-    packages=['xaddpy'],
-    install_requires=['numpy', 'sympy', 'pulp', 'psutil', 'pygraphviz'],
+    packages=find_packages(),
     version='0.1.7',
     license='MIT License',
     description='XADD package in Python',
@@ -17,6 +16,5 @@ setup(
     author='Jihwan Jeong',
     author_email='jiihwan.jeong@gmail.com',
     url='https://github.com/jihwan-jeong/xaddpy',
-    keywords=["xadd", "xadd python", "symbolic diagram"],
     download_url="https://github.com/jihwan-jeong/xaddpy/archive/refs/tags/0.1.7.tar.gz"
 )
